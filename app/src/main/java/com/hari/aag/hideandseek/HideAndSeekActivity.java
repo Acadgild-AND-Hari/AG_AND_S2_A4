8e8ce492edff2fdffa8d97ebd158ff3f49e3453c
harinivaskumar <harinivaskumar@live.com>
2016-12-12 23:33:05 +0530

    Code Commit
    Initial code

---
+++ app/src/main/java/com/hari/aag/hideandseek/HideAndSeekActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.hideandseek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HideAndSeekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hide_and_seek);
+    }
+}
